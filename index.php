bdd5f06de7081c5f6bbcd7f4e1e48a9599a80762
cloud9anti <30364185+cloud9anti@users.noreply.github.com>
2019-11-26 09:40:27 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
 
-        <?php echo "success"; ?>
+
+        <?php
+session_start();
+
+		echo "success"; ?>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<div class="row">
+
+    <div class="container" style="width:600px;">
+
+
+      <div class="col-md-4">
+
+      <p> asd </p>
+
+      </div>
+
+
+
+    </div>
+
+  </div>
