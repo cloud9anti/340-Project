804f3f077cbb6550bcc997cf2934b726b32b3c36
cloud9anti <30364185+cloud9anti@users.noreply.github.com>
2019-11-26 12:18:26 +0700

    Add files via upload

--- studentAdd.php
+++ studentAdd.php
@@ -10,7 +10,7 @@ if (isset ($_POST['name'])  && isset($_POST['details']) && isset($_POST['sku']) 
     //$id = $_GET['id'];
 
 
-		mysqli_query($connection, "INSERT INTO people(first_name, last_name, city) VALUES('$first_name', '$last_name', 'portage')");
+		mysqli_query($connection, "INSERT INTO heroku_7907a8bdd4fde12.people(first_name, last_name, city) VALUES('$first_name', '$last_name', 'portage')");
   
 }
  ?>
