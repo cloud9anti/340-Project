df8d32ef5a6eeafabe6511a94489bbf505b786a7
cloud9anti <30364185+cloud9anti@users.noreply.github.com>
2019-11-26 10:45:12 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -2,7 +2,38 @@
 // Initialize the session
 session_start();
 
-require 'conndb.php';
+//require 'conndb.php';
+
+$dsn = 'mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_7907a8bdd4fde12';
+$host = 'us-cdbr-iron-east-05.cleardb.net';
+$db_name = 'heroku_7907a8bdd4fde12';
+$username = 'b73b7a0286f36e';
+$password = '3f079b57';
+$options = [];
+try {
+$connection = new PDO('mysql:host='.$host.';dbname='.$db_name, $username, $password, $options);
+} catch(PDOException $e) {
+}
+
+
+//user connection (same database)
+$db_server = 'localhost';
+
+
+ 
+// Attempt to connect to MySQL database 
+$link = mysqli_connect($db_server, $username, $password, $db_name);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+
+//track the daily number of orders
+
+static $dailyOrders = 0;
+
 
 //Include people table
 //$sql = 'SELECT * FROM people WHERE active="Active"';
@@ -11,8 +42,10 @@ require 'conndb.php';
 //$statement->execute();
 //$people = $statement->fetchAll(PDO::FETCH_OBJ);
 
-//SELECT * FROM heroku_7907a8bdd4fde12.people;
-
+$sql = 'SELECT * FROM people';
+$statement = $connection->prepare($sql);
+$statement->execute();
+$people = $statement->fetchAll(PDO::FETCH_OBJ);
 
  ?>
 <?php  echo "yay"; //require 'header.php'; ?>
