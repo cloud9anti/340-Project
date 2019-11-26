85cb7b7b1e0ea65c346ee7ccafc6bf272d23148f
cloud9anti <30364185+cloud9anti@users.noreply.github.com>
2019-11-26 11:00:19 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ $db = substr($url["path"], 1);
 $connection = new mysqli($server, $username, $password, $db);
 
 
-$sql = 'SELECT * FROM SELECT * FROM heroku_7907a8bdd4fde12.people';
+$sql = 'SELECT * FROM SELECT * FROM people';
 $statement = $connection->prepare($sql);
 $statement->execute();
 $people = $statement->fetchAll(PDO::FETCH_OBJ);
