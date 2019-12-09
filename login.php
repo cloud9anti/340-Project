1a9c76e6f98ad224ce07826d18c8dfe5fecaefb7
cloud9anti <30364185+cloud9anti@users.noreply.github.com>
2019-12-09 21:41:10 +0700

    Add files via upload

--- login.php
+++ login.php
@@ -67,7 +67,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         .wrapper{ width: 350px; padding: 20px; }
     </style>
 </head>
-<body>
+<body class="bg-light">
     <div class="wrapper">
         <h2>Login</h2>
         <p>Please fill in your credentials to login. </p>
