cc35a5c795c66cd7d23b92cf05e567342700b835
cloud9anti <30364185+cloud9anti@users.noreply.github.com>
2019-12-09 21:52:52 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -2,69 +2,93 @@
 // Initialize the session
 session_start();
 
-require 'conndb.php';
-require 'header.php';
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+
+        
+     
+            // Set parameters
+            $param_username = $username;
 
 
+                
+                    
+
+                        if ($password == 'password' && $username == 'professor'){
+                            // Password is correct, so start a new session
+                            session_start();
+
+							
+
+                            header("location: courses.php");
+							
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = "The username and password you entered was not valid.";
+                        }
+                    
 
 
-$sql = 'SELECT * FROM heroku_7907a8bdd4fde12.people';
-$result = mysqli_query($connection,$sql);
-$people = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
 
- ?>
+?>
 
-
-
+<html lang="en">
 <head>
-	<title>Student List</title>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
 </head>
-<body>
-	<h1>Welcome professor, please select a student!!!!.</h1>
-	
+<body class="bg-light">
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login. </p>
+		<h3> Hint - Username: professor Password: password</h3>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
 
-
-            <h2>Student List</h2>
-		<form action="">
-		  Search by Last name:<br>
-		  <input type="text" name="lastname" value=""><br><br>
-		  <input type="submit" value="Search">
-
-		</form>
-
-
-            <table style="width:100%">
-			<tr>
-			<th> First Name </th>
-			<th> Last Name </th>
-			<th> City </th>
-			
-			</tr>
-	<?php 
-
-
-	while ($people= mysqli_fetch_array($result)) {
-
-						?>
-						
-                <tr>
-                    <td><?php echo $people['first_name']; ?></td>
-                    <td><?php echo $people['last_name']; ?></td>
-                    <td><?php echo $people['city']; ?></td>
-                </tr>
-              
-          			<?php } ?>
-            </table>
-
-
-
-
-<table id="table" align="center"></table>
-
-	  <li >
-        <a href="studentAdd.php">Create New Student </a>
-      </li>
-
+        </form>
+    </div>    
 </body>
-
-
+</html>
