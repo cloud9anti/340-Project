628078fdcde5e63b419e52205476a87c2234a354
cloud9anti <30364185+cloud9anti@users.noreply.github.com>
2019-12-10 13:08:50 +0700

    Add files via upload

--- courses.php
+++ courses.php
@@ -13,7 +13,14 @@ $result = mysqli_query($connection,$sql);
 $people = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
 
-         
+if (isset ($_POST['course'])) {
+
+  $course = $_POST['course'];
+
+
+
+}
+               
 
  ?>
 
