14d3c98f06aee637b46b8267cb258d0403906b15
cloud9anti <30364185+cloud9anti@users.noreply.github.com>
2019-12-10 18:58:59 +0700

    Add files via upload

--- gradeStudents.php
+++ gradeStudents.php
@@ -9,7 +9,7 @@ require 'header.php';
 $courseName = $_POST['course']; 
 $courseID = $_POST['course_id']; 
 $studentID = $_POST['student_id']; 
-$sql = "SELECT * FROM heroku_7907a8bdd4fde12.enrolledcourses WHERE student_id = '$studentID'";
+$sql = "SELECT * FROM heroku_7907a8bdd4fde12.grade WHERE course_id = '$courseID'";
 $result = mysqli_query($connection,$sql);
 $people = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
@@ -24,30 +24,31 @@ $people = mysqli_fetch_array($result, MYSQLI_ASSOC);
 	<h1>Viewing Student ID: <?php echo $_POST['student_id']; ?> 's grades.</h1>
 	
 
+
         <style>
             table {
                 border: 1px solid black;
             }
         </style>
 
-            <table style="width:100%">
+            <table style="width:90%">
 			<tr>
-			<th> Student ID </th>
-
+			<th> Assignment Name </th>
+			<th> Assignment Type </th>
 			
 			</tr>
 			
 			
-			
+			<form method="post" action = "gradeStudents.php">
                 <tr>
-                    <td><?php echo $people['people_id']; ?></td>
+                    <td><?php echo $people['grade_name']; ?></td>
+                    <td><?php echo $people['grade_type']; ?></td>
+					
 					<input type="hidden" name="course" value="<?= $courseName?>"> 
 					<input type="hidden" name="course_id" value="<?= $courseID?>"> 
-					<input type="hidden" name="student_id" value="<?= $people['student_id']?>"> 				
-					<td><input href="gradeStudents.php" value="View Grades" type="submit"></input></td>
-					<td><button type="button">EDIT</button></td>
-					<td><button type="button">DELETE</button></td>
                 </tr>
+              </form>			
+
 	<?php 
 	
 	
@@ -57,15 +58,18 @@ $people = mysqli_fetch_array($result, MYSQLI_ASSOC);
 	while ($people= mysqli_fetch_array($result)) {
 
 						?>
-						
+			<form method="post" action = "gradeStudents.php">
                 <tr>
-                    <td><?php echo $people['people_id']; ?></td>
-
+                    <td><?php echo $people['grade_name']; ?></td>
+                    <td><?php echo $people['grade_type']; ?></td>
+					
+					<input type="hidden" name="course" value="<?= $courseName?>"> 
+					<input type="hidden" name="course_id" value="<?= $courseID?>"> 
                 </tr>
-              
+              </form>			
+
           			<?php } ?>
             </table>
-
 
 
 
