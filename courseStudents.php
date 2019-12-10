06a1fa852c4273edb2a94c7de70252cac7e91667
cloud9anti <30364185+cloud9anti@users.noreply.github.com>
2019-12-10 21:43:53 +0700

    Add files via upload

--- courseStudents.php
+++ courseStudents.php
@@ -25,7 +25,12 @@ if (isset($_POST['btnDelete'])) {
 
   header("location: gradeStudents.php");
 
+  $deleteID = $_POST['deleteID'];
+  
+  echo "testing";
 
+ mysqli_query($connection, "DELETE FROM heroku_7907a8bdd4fde12.course WHERE course_id = '$deleteID'");
+   header("Location: courses.php");
 
 }
  ?>
