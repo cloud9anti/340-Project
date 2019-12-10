80a3bbddd207e22f4a451f80c841fc01caef1705
cloud9anti <30364185+cloud9anti@users.noreply.github.com>
2019-12-10 15:28:15 +0700

    Add files via upload

--- home.php
+++ home.php
@@ -50,7 +50,12 @@ $people = mysqli_fetch_array($result, MYSQLI_ASSOC);
 			</tr>
 	<?php 
 
-
+                <tr>
+                    <td><?php echo $people['first_name']; ?></td>
+                    <td><?php echo $people['last_name']; ?></td>
+                    <td><?php echo $people['city']; ?></td>
+                </tr>
+              
 	while ($people= mysqli_fetch_array($result)) {
 
 						?>
