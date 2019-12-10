3b892795f8d838731c17eb33b4dee3c647215633
cloud9anti <30364185+cloud9anti@users.noreply.github.com>
2019-12-10 22:09:51 +0700

    Add files via upload

--- courseStudents.php
+++ courseStudents.php
@@ -11,12 +11,19 @@ $courseID = $_POST['course_id'];
 $sql = "SELECT * FROM heroku_7907a8bdd4fde12.enrolledcourses WHERE course_id = '$courseID'";
 $result = mysqli_query($connection,$sql);
 $people = mysqli_fetch_array($result, MYSQLI_ASSOC);
- if (isset ($_POST['student_id'])) {
+
+if (isset ($_POST['btnDelete'])) {
+
+  header("location: delete.php");
+  
+
+} else if (isset ($_POST['student_id'])) {
 
   header("location: gradeStudents.php");
 
 }
  ?>
+
 
 
 
@@ -80,7 +87,51 @@ $people = mysqli_fetch_array($result, MYSQLI_ASSOC);
             </table>
 
 
+           <table style="width:90%">
+			<tr>
+			<th> Student ID </th>
+			<th> View </th>
+			<th> Edit </th>
+			<th> Delete </th>
 
+			
+			</tr>
+			
+			
+			<form method="post" action = "gradeStudents.php">
+                <tr>
+                    <td><?php echo $people['people_id']; ?></td>
+					<input type="hidden" name="course" value="<?= $courseName?>"> 
+					<input type="hidden" name="course_id" value="<?= $courseID?>"> 
+					<input type="hidden" name="student_id" value="<?= $people['people_id']?>"> 				
+					<td><input href="gradeStudents.php" value="View Grades" type="submit"></input></td>
+					<td><button type="button">EDIT</button></td>
+					<td><button type="button">DELETE</button></td>
+                </tr>
+              </form>			
+
+	<?php 
+	
+	
+
+
+
+	while ($people= mysqli_fetch_array($result)) {
+
+						?>
+			<form method="post" action = "gradeStudents.php">
+                <tr>
+                    <td><?php echo $people['people_id']; ?></td>
+					<input type="hidden" name="course" value="<?= $courseName?>"> 
+					<input type="hidden" name="course_id" value="<?= $courseID?>"> 
+					<input type="hidden" name="student_id" value="<?= $people['people_id']?>"> 				
+					<td><input href="gradeStudents.php" value="View Grades" type="submit"></input></td>
+					<td><button type="button">EDIT</button></td>
+					<td><button type="button">DELETE</button></td>
+                </tr>
+              </form>
+          			<?php } ?>
+            </table>
 
 <table id="table" align="center"></table>
 
