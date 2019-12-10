c5e0684c6c5e61fa8269a54cd9e5752aad494f84
cloud9anti <30364185+cloud9anti@users.noreply.github.com>
2019-12-10 13:59:16 +0700

    Add files via upload

--- courses.php
+++ courses.php
@@ -8,7 +8,7 @@ require 'header.php';
 
 
 
-$sql = 'SELECT * FROM heroku_7907a8bdd4fde12.people';
+$sql = 'SELECT * FROM heroku_7907a8bdd4fde12.course';
 $result = mysqli_query($connection,$sql);
 $people = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
@@ -58,8 +58,8 @@ if (isset ($_POST['course'])) {
 								?>
 								
 						<tr>
-							<td><?php echo $people['first_name']; ?></td>
-							<td><?php echo $people['people_id']; ?></td>
+							<td><?php echo $people['course_name']; ?></td>
+							<td><?php echo $people['course_id']; ?></td>
 							<td> HELLO </td>
 						</tr>
 									<?php } ?>
