6aa8ca7f284bf3741681b34ae71af958852d0449
cloud9anti <30364185+cloud9anti@users.noreply.github.com>
2019-12-10 14:25:47 +0700

    Add files via upload

--- courses.php
+++ courses.php
@@ -62,7 +62,7 @@ if (isset ($_POST['course'])) {
 							<td><?php echo $people['course_name']; ?></td>
 							<td><?php echo $people['department']; ?></td>
 
-							<input type="hidden" name="Product_ID" value="<?= $products['Product_ID']?>"> 
+							<input type="hidden" name="course" value="<?= $people['course_id']?>"> 
 							<td><input href="courseStudents.php" type="submit"></input></td>
 							<td><button type="button">EDIT</button></td>
 							<td><button type="button">DELETE</button></td>
