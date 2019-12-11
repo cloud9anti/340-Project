220363a07c0c698a5e11616d4ef88ffc509885b7
cloud9anti <30364185+cloud9anti@users.noreply.github.com>
2019-12-11 15:17:31 +0700

    Add files via upload

--- delete.php
+++ delete.php
@@ -2,13 +2,20 @@
 require 'conndb.php';
 require 'header.php';
 
-
+//delete course
   $deleteID = $_POST['btnDelete'];
   
   echo "testing";
 
  mysqli_query($connection, "DELETE FROM heroku_7907a8bdd4fde12.course WHERE course_id = '$deleteID'");
    header("Location: courses.php");
+   
+//delete assignment
+  $deleteID = $_POST['btnDelete2'];
+  
+  echo "testing";
 
+ mysqli_query($connection, "DELETE FROM heroku_7907a8bdd4fde12.grade WHERE grade_id = '$deleteID'");
+   header("Location: assignments.php");
  ?>
  
